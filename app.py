475f1f70d9e3f707fbac8477f5df756a1b089fdb
Albert Árboles <164230446+tinips@users.noreply.github.com>
2025-03-15 16:21:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Flask running on Azure!"
+    return "Hello, Flask running on Azure!!"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8000)
